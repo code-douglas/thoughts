2236436d026006870f3f481edea625206e03b50f
codin <douglasd3v.contato@gmail.com>
2025-02-16 11:53:09 -0300

    Importação/inicialização de pacotes, conexão com o banco de dados, importação de arquivo para conexão

--- main.mjs
+++ main.mjs
@@ -1,5 +1,24 @@
+// Packages import
 import express from 'express';
+import expbhs from 'express-handlebars';
+import session from 'express-session';
+import sessionFileStore from 'session-file-store';
+import flash from 'express-flash';
 
+// DB connection import
+import connection from './db/connection.mjs';
+
+// Initialize packages
 const app = express();
+const FileStore = sessionFileStore(session);
 
-app.listen(3003);
+// Initialize database
+connection
+  .sync()
+  .then(() => {
+    app.listen(3003);
+    console.log('Servidor rodando: http://localhost:3003');
+  })
+  .catch(
+    (error) => console.log(`Falha na conexao: ${error}`)
+  );
