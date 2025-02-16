3ef74be274eb2bb3e95541f8b3b70f8366617e47
codin <douglasd3v.contato@gmail.com>
2025-02-16 19:14:06 -0300

    Add: Rota para login

--- routes/authRoutes.mjs
+++ routes/authRoutes.mjs
@@ -3,12 +3,10 @@ const router = express.Router();
 
 import AuthController from '../controllers/AuthController.mjs';
 
-console.log(AuthController);
-
 router.get('/login', AuthController.login);
+router.post('/login', AuthController.loginPost);
 router.get('/register', AuthController.register);
 router.post('/register', AuthController.registerPost);
 router.get('/logout', AuthController.logout);
-
 
 export default router;
