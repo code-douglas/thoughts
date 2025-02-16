20a3708cc7109647c35470065f3621ca165768bf
codin <douglasd3v.contato@gmail.com>
2025-02-16 16:44:01 -0300

    Adicionando rota de POST para criação de usuário

--- routes/authRoutes.mjs
+++ routes/authRoutes.mjs
@@ -7,5 +7,7 @@ console.log(AuthController);
 
 router.get('/login', AuthController.login);
 router.get('/register', AuthController.register);
+router.post('/register', AuthController.registerPost);
+
 
 export default router;
