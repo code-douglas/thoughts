fcc0832c4f2a55ea72f1f6d13013cc01fc1a04ec
codin <douglasd3v.contato@gmail.com>
2025-02-16 18:56:13 -0300

    Criando rota de logout

--- routes/authRoutes.mjs
+++ routes/authRoutes.mjs
@@ -8,6 +8,7 @@ console.log(AuthController);
 router.get('/login', AuthController.login);
 router.get('/register', AuthController.register);
 router.post('/register', AuthController.registerPost);
+router.get('/logout', AuthController.logout);
 
 
 export default router;
