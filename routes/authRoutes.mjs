ff12d857ea148b85fd017452b014cf72cdd5be19
codin <douglasd3v.contato@gmail.com>
2025-02-16 15:36:22 -0300

    Reparando erro na rota de autenticação

--- routes/authRoutes.mjs
+++ routes/authRoutes.mjs
@@ -1,6 +1,9 @@
 import express from 'express';
 const router = express.Router();
+
 import AuthController from '../controllers/AuthController.mjs';
+
+console.log(AuthController);
 
 router.get('/login', AuthController.login);
 router.get('/register', AuthController.register);
