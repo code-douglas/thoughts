061ca80e3fa3200910e5906225dc541927870260
codin <douglasd3v.contato@gmail.com>
2025-02-17 11:04:28 -0300

    Add: Rota para dashboard, com importação do middleware

--- routes/toughtsRoutes.mjs
+++ routes/toughtsRoutes.mjs
@@ -1,7 +1,9 @@
 import express from 'express';
 const router = express.Router();
 import ToughtsController from '../controllers/ToughtsController.mjs';
+import checkUserAuth from '../middlewares/checkUserAuth.mjs';
 
+router.get('/dashboard', checkUserAuth, ToughtsController.dashboard);
 router.get('/', ToughtsController.showToughts);
 
 export default router;
