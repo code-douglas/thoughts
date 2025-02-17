f57c7e229fc8554b6a51571ff45f1887f2a91f4b
codin <douglasd3v.contato@gmail.com>
2025-02-17 11:37:40 -0300

    Rota de render, com middleware de check auth

--- routes/toughtsRoutes.mjs
+++ routes/toughtsRoutes.mjs
@@ -3,6 +3,7 @@ const router = express.Router();
 import ToughtsController from '../controllers/ToughtsController.mjs';
 import checkUserAuth from '../middlewares/checkUserAuth.mjs';
 
+router.get('/add', checkUserAuth, ToughtsController.createTought);
 router.get('/dashboard', checkUserAuth, ToughtsController.dashboard);
 router.get('/', ToughtsController.showToughts);
 
