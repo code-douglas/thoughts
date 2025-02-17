ccf0a1ce705bcc1e07dee6e53f2c7ef81723ebd1
codin <douglasd3v.contato@gmail.com>
2025-02-17 11:02:28 -0300

    Adição da página de dashboard

--- controllers/ToughtsController.mjs
+++ controllers/ToughtsController.mjs
@@ -5,4 +5,7 @@ export default class ToughtsController {
   static async showToughts(req, res) {
     res.render('toughts/home');
   }
+  static async dashboard(req, res) {
+    res.render('toughts/dashboard');
+  }
 }
