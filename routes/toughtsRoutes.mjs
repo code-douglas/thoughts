8aee625a6897c484fe57c29c63ba9f27f6d9ca53
codin <douglasd3v.contato@gmail.com>
2025-02-18 10:39:40 -0300

    Add: Rota de post para remoção

--- routes/toughtsRoutes.mjs
+++ routes/toughtsRoutes.mjs
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/add', checkUserAuth, ToughtsController.createTought);
 router.post('/add', checkUserAuth, ToughtsController.createToughtSave);
 router.get('/dashboard', checkUserAuth, ToughtsController.dashboard);
+router.post('/remove', checkUserAuth, ToughtsController.removeTought);
 router.get('/', ToughtsController.showToughts);
 
 export default router;
