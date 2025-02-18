8a0014f1d27bf6304bd9ffeca8c929e5fba9db3f
codin <douglasd3v.contato@gmail.com>
2025-02-18 09:44:56 -0300

    Add: Rota para post

--- routes/toughtsRoutes.mjs
+++ routes/toughtsRoutes.mjs
@@ -4,6 +4,7 @@ import ToughtsController from '../controllers/ToughtsController.mjs';
 import checkUserAuth from '../middlewares/checkUserAuth.mjs';
 
 router.get('/add', checkUserAuth, ToughtsController.createTought);
+router.post('/add', checkUserAuth, ToughtsController.createToughtSave);
 router.get('/dashboard', checkUserAuth, ToughtsController.dashboard);
 router.get('/', ToughtsController.showToughts);
 
